02312593dec684ff2bdb13f356cdbfb93327485c
Pe4EHKA <Pe4EHKA.COM@yandex.ru>
2023-03-06 10:45:56 +0500

    Создал конструкторы, деструктор и место clear.

--- OOP3/main.cpp
+++ OOP3/main.cpp
@@ -1,14 +1,34 @@
-//
-//  main.cpp
-//  OOP3
-//
-//  Created by Валера Батурин on 06.03.2023.
-//
-
 #include <iostream>
+#include <cassert>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+template <class T>
+class MyStorage {
+private:
+    int size;
+    T* data;
+public:
+    
+    MyStorage() : size(0), data(nullptr) {}
+    
+    MyStorage(int size) : size(size) {
+        assert(size >= 0);  // Если условие не выполняется = ошибка.
+        if (size > 0) {
+            data = new T[size];
+        }
+    }
+    
+    ~MyStorage() {
+        delete data;
+    }
+    
+    void clear() {
+        delete data;
+        data = nullptr;  // Чтобы не указывал на освобожденную память(никуда).
+        size = 0;
+    }
+};
+
+int main() {
     std::cout << "Hello, World!\n";
     return 0;
 }
